eeedbe507446ed2ff58e056f8e0b7c6468716a0d
Dragos Strugar <itsdragos@gmail.com>
2018-09-26 22:56:02 +0300

    Add week 6

---
+++ weeksix/exc3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+#define ALWAYS_TRUE 1
+
+void sigint_func(int nmr);
+
+// SIGINT - ctrl + c
+int main() {
+    signal(SIGINT, sigint_func);
+    while (ALWAYS_TRUE) {
+        sleep(2);
+    }
+}
+
+void sigint_func(int nmr) {
+    if (nmr == SIGINT)
+        printf("Received SIGINT Signal!");
+}
